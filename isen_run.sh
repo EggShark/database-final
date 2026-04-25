98840e74b81f4c49f7edc72196e8bc29893f5f53
EggShark <eggshark@eggshark.dev>
2026-04-25 16:15:28 -0600

    fixed type in isen_run.sh

--- isen_run.sh
+++ isen_run.sh
@@ -5,7 +5,7 @@ cargo b
 
 # make sure you have isen_jumpbox in your ~/.ssh/config
 # uplaods geojso (maybe make skipable)
-scp ALPRs.geojson isen_jumbox:database-final/ALPRs.geojson
+scp ALPRs.geojson isen_jumpbox:database-final/ALPRs.geojson
 scp target/debug/database-final isen_jumpbox:database-final/database_uploader
 
 ssh isen_jumpbox 'cd database-final; ./database_uploader'
